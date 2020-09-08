e2eec255f9bc25435c3fabfcfe9d91e10f824de9
marcadagua <marcadagua@HOME>
2020-09-08 01:08:29 +0100

    Modify DataBase

--- ShopCet47.Web/Data/Entities/Product.cs
+++ ShopCet47.Web/Data/Entities/Product.cs
@@ -10,7 +10,8 @@ namespace ShopCet47.Web.Data.Entities
     {
         public int Id { get; set; }
 
-
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain a maximum {1} characters")]
+        [Required]
         public string Name { get; set; }
 
 
@@ -23,10 +24,10 @@ namespace ShopCet47.Web.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Availabe?")]
